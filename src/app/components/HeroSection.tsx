d2d7d6e852e214a80cf50cb0520af43c29db3a9b
GCODESOfficial <godsgiftetuk2001@gmail.com>
2025-07-19 10:05:38 +0100

    add

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -4,10 +4,19 @@ import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <section className="relative bg-black text-white py-20 px-4 flex flex-col items-center text-center overflow-hidden">
-      {/* Spotlight effect */}
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-64 h-64 bg-gradient-to-b from-white/20 to-transparent rounded-full blur-3xl"></div>
+    <section className="relative bg-black text-white py-60 px-4 flex justify-center items-center text-center overflow-hidden">
+  {/* Founder badge & image */}
+  <div className="">
+        <Image
+          src="/images/chrisjohn.svg" // store your image in public/profile.png
+          alt="Founder"
+          width={150}
+          height={150}
+          className=""
+        />
+      </div>
 
+<div>
       {/* Main Heading */}
       <h1 className="text-4xl md:text-6xl font-bold mb-2">
         When you win <br /> in your mind,
@@ -20,17 +29,8 @@ export default function HeroSection() {
       <p className="max-w-xl text-[#B0B0B0] mb-12">
         I build People, Brands and Great User Experiences for Global Brands and Intentional Individuals.
       </p>
-
-      {/* Founder badge & image */}
-      <div className="relative flex flex-col items-center">
-        <Image
-          src="/images/profile.svg" // store your image in public/profile.png
-          alt="Founder"
-          width={150}
-          height={150}
-          className=""
-        />
       </div>
+      
     </section>
   );
 }
